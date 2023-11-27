3627dd28057c81f6ee3ec5996b6997490ba10d37
Natasa <natasa.ristic@hotmail.com>
2023-11-27 11:51:29 +0100

    update article

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
