e45000820f89e73eecddb7127a3443770210b1fc
Natasa <natasa.ristic@hotmail.com>
2023-11-28 12:44:17 +0100

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,17 @@
 class ArticlesController < ApplicationController
-  def index
-
+  def new
+  @article = Article.new
   end
+  def create
+    pp params
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  private
 
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
