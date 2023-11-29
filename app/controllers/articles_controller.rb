f0c5a114cb2c6f8a13c25ff171ffb8f1f3d0091a
Natasa <natasa.ristic@hotmail.com>
2023-11-29 13:21:16 +0100

    update delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,11 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted.'
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
