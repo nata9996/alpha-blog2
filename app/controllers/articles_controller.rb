00789bed6ec290709f8dd30e43bb14998f6d7011
Natasa <natasa.ristic@hotmail.com>
2023-11-29 12:34:16 +0100

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,37 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
   @article = Article.new
   end
-  def create
-    pp params
-    #render plain: params[:article].inspect
-    @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+  def edit
+  @article = Article.find(params[:id])
   end
-  private
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = @article.errors.full_messages
+      render 'new'
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = @article.errors.full_messages
+      render 'edit'
+    end
 
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
